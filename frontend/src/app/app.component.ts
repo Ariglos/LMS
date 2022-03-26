c84009012ff7c0657e9cee3e48cd24876db33309
Ariglos <piotr4165@wp.pl>
2022-03-26 19:50:56 +0100

    LMS-10 Footer

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -1,6 +1,4 @@
 import { Component } from '@angular/core';
-import { ThemeService } from './core/services/theme.service';
-import { Theme } from './core/enums/Theme';
 
 @Component({
   selector: 'app-root',
@@ -8,17 +6,5 @@ import { Theme } from './core/enums/Theme';
   styleUrls: ['./app.component.scss'],
 })
 export class AppComponent {
-  title = 'frontend';
-
-  constructor(private themeService: ThemeService) {}
-
-  onClick() {
-    let random = Math.random();
-    let theme = Theme.Light;
-    if (random > 0.5) {
-      theme = Theme.Dark;
-    }
-
-    this.themeService.switchTheme(theme);
-  }
+  constructor() {}
 }
